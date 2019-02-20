82cd941e8119a6d0260108a35724f8bcf5d9b99a
hanfeng11 <2712094616@qq.com>
2019-02-20 16:45:54 +0800

    day03_日常练习

--- rr/ww.py
+++ rr/ww.py
@@ -1,6 +1,45 @@
-print("sdhashd")
-print("hanfeng123")
-print("hanfeng1234")
-print("hanfeng12345")
-
-
+# print("sdhashd")
+# print("hanfeng123")
+# print("hanfeng1234")
+# print("hanfeng12345")
+#
+# import os
+# os.chdir("./")
+# import os
+# os.mkdir("abc")
+# for i in range(5):
+#     print(i)
+# else:
+#     print("end")
+# for n in range(2,10):
+#     print(n)
+#     if n==3:
+#         break
+#     elif n>3:
+#         print("love")
+#求阶乘之和1
+# sum=0
+# count=1
+# num=int(input("请输入一个数:"))
+# for i in range(1,num+1):
+#     count=count*i
+#     sum+=count
+# print("阶乘之和:",sum)
+# 求阶乘之和2
+# import math
+# sum=0
+# num=int(input("请输入一个数字:"))
+# for i in range(1,num+1):
+#     f=math.factorial(i)
+#     sum+=f
+# print("阶乘之和为:",sum)
+# 有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+# for i in range(1,5):
+#     for j in range(1,5):
+#         for k in range(1,5):
+#             if(i!=k) and (i!=j) and (j!=k):
+#                 print(i,j,k)
+# 一行语句if判断语句
+# a = 5 if 5 > 6 else (6 if 3 > 2 else 5)
+# print(a)
+#函数
