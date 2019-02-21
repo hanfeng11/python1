53a2fba105ebfebb902ffd288441d4d47b184236
hanfeng11 <2712094616@qq.com>
2019-02-21 11:49:31 +0800

    随机生成数字，猜大小

---
+++ rr/day04_lianxi5.py
@@ -0,0 +1,16 @@
+# 随机生成数字，猜大小
+import random
+b=random.randint(1,100)
+a=int(input("请输入一个数字（1-100）:"))
+while 1:
+
+    if a==b:
+        print("恭喜你猜对了!")
+        break
+    elif a<b:
+        print("猜小了")
+        a = int(input("请输入一个数字（1-100）:"))
+    elif a>b:
+        print("猜大了")
+        a = int(input("请输入一个数字（1-100）:"))
+
